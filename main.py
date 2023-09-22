605259f6820495e328dae424c1379c5afbddd80b
Sayan Saha <sayan@sourcewiz.co>
2023-09-22 22:56:16 +0530

    feat: improvement

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from models import Report
 from db import SESSION
 from task import ReportGenerateTask
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, FileResponse
+from pathlib import Path
 
 app = FastAPI()
 
@@ -36,6 +37,13 @@ def get_report(report_id: str):
     try:
         report = SESSION.query(Report).filter(Report.id == report_id).first()
         SESSION.close()
+        if report.status == "Complete":
+            return FileResponse(
+                report.file,
+                media_type="application/octet-stream",
+                filename=Path(report.file).name,
+            )
+
         return JSONResponse(status_code=200, content={"status": report.status})
     except Exception as e:
         SESSION.close()
--- task.py
+++ task.py
@@ -3,7 +3,7 @@ from env import REDIS_URL
 from db import SESSION
 from models import *
 from schema import *
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 from pytz import timezone, utc
 import pandas as pd
 
@@ -12,6 +12,10 @@ app = Celery(
     backend=REDIS_URL,
     broker=REDIS_URL,
 )
+
+
+def batch(list_data, bs):
+    return [list_data[i : i + bs] for i in range(0, len(list_data), bs)]
 
 
 class ReportGenerateTask(Task):
@@ -23,42 +27,45 @@ class ReportGenerateTask(Task):
 
     def run(self, report_id):
         report = SESSION.query(Report).where(Report.id == report_id).first()
-        all_stores = SESSION.query(StoreTimezone).all()
+        all_stores = SESSION.query(StoreTimezone).limit(300).all()
+        store_timezone = {i.store_id: i.timezone_str for i in all_stores}
+
+        all_batch_store_ids = batch([i.store_id for i in all_stores], 500)
         current_time = datetime(2023, 1, 25, 18, 13, 22, 479220)
         final_data = []
-        for store in all_stores:
-            _data = {}
-            business_hours = self.get_business_hours(store_ids=[store.store_id])
-            store_status = self.get_store_status(store_ids=[store.store_id])
-
-            uptime = self.get_uptime_or_downtime(
-                current_time,
-                business_hours,
-                store_status,
-                tzone=store.timezone_str,
-                status_type="uptime",
-            )
-            downtime = self.get_uptime_or_downtime(
-                current_time,
-                business_hours,
-                store_status,
-                tzone=store.timezone_str,
-                status_type="downtime",
-            )
-            _data["store_id"] = store.store_id
-            _data["uptime_last_hour(in minutes)"] = uptime.get("last_hour")
-            _data["uptime_last_day(in hours)"] = uptime.get("last_day")
-            _data["uptime_last_week(in hours)"] = uptime.get("last_week")
-            _data["downtime_last_hour(in minutes)"] = downtime.get("last_hour")
-            _data["downtime_last_day(in hours)"] = downtime.get("last_day")
-            _data["downtime_last_week(in hours)"] = downtime.get("last_week")
-            final_data.append(_data)
-            # break
-
+        for batch_store_ids in all_batch_store_ids:
+            business_hours = self.get_business_hours(store_ids=batch_store_ids)
+            store_status = self.get_store_status(store_ids=batch_store_ids)
+            for _store_id in batch_store_ids:
+                _data = {}
+                uptime = self.get_uptime_or_downtime(
+                    current_time=current_time,
+                    business_hours=business_hours.get(_store_id, []),
+                    store_status=store_status.get(_store_id, []),
+                    tzone=store_timezone.get(_store_id, "America/Chicago"),
+                    status_type="uptime",
+                )
+                downtime = self.get_uptime_or_downtime(
+                    current_time=current_time,
+                    business_hours=business_hours.get(_store_id, []),
+                    store_status=store_status.get(_store_id, []),
+                    tzone=store_timezone.get(_store_id, "America/Chicago"),
+                    status_type="downtime",
+                )
+                _data["store_id"] = _store_id
+                _data["uptime_last_hour(in minutes)"] = uptime.get("last_hour")*60
+                _data["uptime_last_day(in hours)"] = uptime.get("last_day")
+                _data["uptime_last_week(in hours)"] = uptime.get("last_week")
+                _data["downtime_last_hour(in minutes)"] = downtime.get("last_hour")*60
+                _data["downtime_last_day(in hours)"] = downtime.get("last_day")
+                _data["downtime_last_week(in hours)"] = downtime.get("last_week")
+                final_data.append(_data)
         df = pd.DataFrame(final_data)
-        df.to_csv("test.csv", header=True, index=None)
+        file_path = f"Static/Report_{report_id}.csv"
+        df.to_csv(file_path, header=True, index=None)
 
-        report.status = "Completed"
+        report.status = "Complete"
+        report.file = file_path
 
         SESSION.add(report)
         SESSION.commit()
@@ -68,7 +75,14 @@ class ReportGenerateTask(Task):
         store_status = (
             SESSION.query(StoreStatus).filter(StoreStatus.store_id.in_(store_ids)).all()
         )
-        return StoreStatusSchema(many=True).dump(store_status)
+        data = StoreStatusSchema(many=True).dump(store_status)
+        grouped_data = {}
+        for i in data:
+            if i["store_id"] not in grouped_data:
+                grouped_data[i["store_id"]] = []
+            grouped_data[i["store_id"]].append(i)
+
+        return grouped_data
 
     def get_business_hours(self, store_ids):
         business_hours = (
@@ -76,7 +90,14 @@ class ReportGenerateTask(Task):
             .filter(BusinessHours.store_id.in_(store_ids))
             .all()
         )
-        return BusinessHoursSchema(many=True).dump(business_hours)
+        data = BusinessHoursSchema(many=True).dump(business_hours)
+        grouped_data = {}
+        for i in data:
+            if i["store_id"] not in grouped_data:
+                grouped_data[i["store_id"]] = []
+            grouped_data[i["store_id"]].append(i)
+
+        return grouped_data
 
     def get_uptime_or_downtime(
         self, current_time, business_hours, store_status, tzone, status_type
@@ -109,32 +130,41 @@ class ReportGenerateTask(Task):
         return status_data
 
     def get_timestamp_object(self, ts):
-        return datetime.strptime(ts, '%Y-%m-%dT%H:%M:%S.%f')
+        return datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S.%f")
 
     def is_in_business_hours(self, _timestamp, business_hours, tzone):
         tz = timezone(tzone)
-        for row in business_hours:
-            day, start_time_local, end_time_local = (
-                row.get("day"),
-                row.get("start_time_local"),
-                row.get("end_time_local"),
-            )
-            start_time_utc = (
-                tz.localize(datetime.strptime(start_time_local, "%H:%M:%S"))
-                .astimezone(utc)
-                .time()
-            )
-            end_time_utc = (
-                tz.localize(datetime.strptime(end_time_local, "%H:%M:%S"))
-                .astimezone(utc)
-                .time()
-            )
+        day_business_hour = [i for i in business_hours if i["day"]==_timestamp.weekday()]
 
+        if day_business_hour:
+            for row in business_hours:
+                day, start_time_local, end_time_local = (
+                    row.get("day"),
+                    row.get("start_time_local"),
+                    row.get("end_time_local"),
+                )
+                start_time_utc = (
+                    tz.localize(datetime.strptime(start_time_local, "%H:%M:%S"))
+                    .astimezone(utc)
+                    .time()
+                )
+                end_time_utc = (
+                    tz.localize(datetime.strptime(end_time_local, "%H:%M:%S"))
+                    .astimezone(utc)
+                    .time()
+                )
+
+                if (
+                    start_time_utc <= _timestamp.time() <= end_time_utc
+                ):
+                    return True
+        else:
+            start_time_utc = time(0, 0, 0)  
+            end_time_utc = time(23, 59, 59)
             if (
-                day == _timestamp.weekday()
-                and start_time_utc <= _timestamp.time() <= end_time_utc
-            ):
-                return True
+                    start_time_utc <= _timestamp.time() <= end_time_utc
+                ):
+                    return True
         return False
 
 
